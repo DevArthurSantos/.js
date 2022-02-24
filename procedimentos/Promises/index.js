c583532facc6d4716e4ea563fc69099b12c828e5
DevArthurSantos <arthursantos.s@hotmail.com>
2022-02-24 12:32:21 -0300

    Promises++

--- procedimentos/Promises/index.js
+++ procedimentos/Promises/index.js
@@ -13,18 +13,43 @@ function inserirUsuarios(nome) {
   let promese = new Promise(function (resolvido, rejeitado) {
     usuarios.push(nome);
     let error = true
-    if (!error) {
-      resolvido();
-    } else {
-      rejeitado({msg: "rejeitado"});
+    if (error) {
+      rejeitado(new Error ("Rejeitado: Promessa1"))
     }
+    resolvido({usuarios});
   });
   return promese;
 }
 
-function ListarUsuarios(){
-   console.log(usuarios)
+function ListarUsuarios(deta){
+  console.log(deta.usuariosssss)
 }
 inserirUsuarios("a").then(ListarUsuarios).catch((error) =>{
-   console.log(error.msg)
+   console.log(error.message)
 });
+
+///////////////////////////
+function promessa2(){
+  let Data = Promise.resolve("Rejeitado: Promessa2")
+  return Data
+}
+
+promessa2().then(console.log)
+
+/////////////////////////
+
+async function promessa3(){
+  throw new Error ("Rejeitado: Promessa3")
+  return 100
+}
+
+async function init(){
+  
+  try{
+    await promessa3()
+  }
+    catch(error){
+      console.log(error.message)
+    }
+
+}init()
